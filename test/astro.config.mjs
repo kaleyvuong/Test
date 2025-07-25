de48e179456ab9c7060cfc8b3ac017a7e376b51c
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-07-24 21:23:30 -0700

    pls

--- test/astro.config.mjs
+++ test/astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// // @ts-check
+// import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// // https://astro.build/config
+// export default defineConfig({});
+
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
+
+export default defineConfig({
+  output: 'static',
+  adapter: netlify()
+});
